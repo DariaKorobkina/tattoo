fda8da9d101c2b1c534bcb453774ebaab66f8baf
dkorobkina <dkorobkina@justcoded.com>
2017-02-15 15:07:07 +0200

    add img

---
+++ header.php
@@ -0,0 +1,8 @@
+<?php
+	<head>
+	    <meta charset="utf-8">   
+	    <title>Tatoo clinic Zhyvoder</title>
+	    
+	    <?php include_once "font.php"; ?>
+
+	</head>
---
+++ nav.php
@@ -0,0 +1,13 @@
+<a id="nav-toggle" href="#">
+	<span>
+	</span>
+</a>
+<nav id="nav-menu">
+	<ul>
+		<li><a title="Last Works" href="#last-works">Last Works</a></li>
+		<li><a title="Services" href="#services">Services</a></li>
+		<li><a title="Studio" href="#studio">Studio</a></li>
+		<li><a title="Artists" href="#artists">Artists</a></li>
+		<li><a title="Contact Us" href="#contacts">Contact Us</a></li>
+	</ul>
+</nav>
